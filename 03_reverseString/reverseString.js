b2351854c9842736751ea95dabbafd7cf523b642
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-20 23:13:29 +0100

    Solve 03 TOP Javascript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,24 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // split the word into letters
+    const letters = word.split("");
+    let reversedLetters = [];
+    const reversedWords = [];
+
+    // loop over each letter in words
+    for (let i = letters.length - 1; i >= 0; i--) {
+        // if empty space join current reversed letters and push into reversed word list
+        if (letters[i] === " ") {
+            reversedWords.push(reversedLetters.join(""));
+            // reset reversed letters list
+            reversedLetters = [];
+        } else {
+            reversedLetters.push(letters[i]);
+        }
+    }
+    // join current reversed letters and push into reversed word list
+    reversedWords.push(reversedLetters.join(""))
+
+    return reversedWords.join(" ");
 
 };
 
