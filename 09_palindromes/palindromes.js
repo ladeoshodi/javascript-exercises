8eced636f9ad857e937119582e5112124fe6f682
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-21 22:56:40 +0100

    solve 09 TOP javascript exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // only match alphanumeric values
+    const pattern = /[a-zA-Z0-9]/g;
+    let matchedWord = word.match(pattern).join("").toLowerCase();
+
+    let reversedWord = ""
+    for (let i = matchedWord.length - 1; i >= 0; i--) {
+        reversedWord += matchedWord[i];
+    }
+
+    return matchedWord === reversedWord
+
+
 
 };
 
