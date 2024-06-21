96141d1db346374e8688c09ca1ee9b2c4b750328
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-21 01:16:46 +0100

    solve 04 TOP javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
+
+// or
+// const removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
+//
 
 // Do not edit below this line
 module.exports = removeFromArray;
