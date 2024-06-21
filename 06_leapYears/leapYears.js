b35418e2914b2bc792bb293315ea1d53d80dd300
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-21 14:52:09 +0100

    solve 06 TOP javascript exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // check if divisible by 4 but not divisible by 100
+    if (year % 4 === 0 && !(year % 100 === 0)) {
+        return true;
+    // check if divisible by both 100 & 400
+    } else if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    } else {
+        return false
+    }
 
 };
 
