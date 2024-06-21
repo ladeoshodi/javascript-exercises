9d909df33617c8b6dbaf88a54a391125603ece73
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-21 07:44:44 +0100

    solve 05 TOP javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,45 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    // first method
+    // if ((typeof start === "number" && start > 0) && (typeof end === "number" && end > 0)) {
+    //     let sum = 0;
+    //     if (start < end) {
+    //         for (let i = start; i <= end; i++) {
+    //             sum += i;
+    //         }
+    //         return sum
+    //     } else if (start > end) {
+    //         for (let i = start; i >= end; i--) {
+    //             sum += i;
+    //         }
+    //         return sum
+    //     } else {
+    //         return sum += start;
+    //     }
+    // } else {
+    //     return "ERROR"
+    // }
+
+    // using Array.reduce()
+    if ((typeof start === "number" && start > 0) && (typeof end === "number" && end > 0)) {
+        // create a range between start and stop as an array
+        let integerArray = [];
+        if (start < end) {
+            for (let i = start; i <= end; i++) {
+                integerArray.push(i);
+            }
+        } else if (start > end) {
+            for (let i = start; i >= end; i--) {
+                integerArray.push(i);
+            }
+        } else {
+            integerArray.push(start)
+        }
+
+        // Let's use the reduce function to sum up the values in the array
+        return integerArray.reduce((sum, integer) => sum += integer);
+    } else {
+        return "ERROR"
+    }
 
 };
 
