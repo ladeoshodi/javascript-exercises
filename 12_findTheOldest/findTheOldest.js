6f85fc65a1b77516f745610d9498aa809dbcb2a8
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-06-22 13:06:42 +0100

    solve 12 TOP javascript exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = [];
 
+    // Calculate the age of the person and store in an array
+    people.forEach((person) => {
+        person["age"] = person.yearOfDeath !== undefined ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+
+        ages.push(person.age)
+    })
+
+    // Get the max age 
+    oldestAge = Math.max(...ages)
+
+    // Filter the person with the Oldest age
+    return people.filter((person) => person.age === oldestAge)[0]
 };
 
 // Do not edit below this line
